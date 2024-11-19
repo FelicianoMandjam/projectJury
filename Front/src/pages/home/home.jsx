18c330ca71268cac931000623066c0644782c649
feliciano <felicianomandja@gmail.com>
2024-11-19 16:32:58 +0100

    Ajout des socket BACK et FRONT

--- Front/src/pages/home/home.jsx
+++ Front/src/pages/home/home.jsx
@@ -1,93 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-// REACT BOOTSRAP
-import Carousel from "react-bootstrap/Carousel";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
+// URL
+import { URL } from "../../URL/URL";
+
+// Socket
+import { io } from "socket.io-client";
+
+// Initialization de la connexion WebSocket coté back
+const socket = io("http://localhost:3001");
 
 const Home = () => {
+  // Pour afficher les articles socket.IO d'abort
+  const [post, setPost] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [content, setContent] = useState({
+    name: "",
+    content: "",
+  });
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        console.log("Entree dans la requete ");
+        const { data } = await axios.get(URL.POST_GET_ALL);
+        console.log(data);
+        setPost(data);
+      } catch (error) {
+        console.log("Erreur lors de la requete des posts ");
+      }
+    };
+    fetchPost();
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setContent((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      await axios.post(URL.POST_ADD, content);
+    } catch (error) {
+      console.log("Erreur  posting the post", error);
+    }
+    setIsLoading(false);
+  };
   return (
     <>
-      <h1>Page d'accueil</h1>
-      <div className="carrossel">
-        <Carousel data-bs-theme="dark">
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://cdn.pixabay.com/photo/2017/08/07/14/02/man-2604149_1280.jpg"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h5>First slide label</h5>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              <Button variant="outline-danger" size="lg">
-                Lire
-              </Button>{" "}
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://cdn.pixabay.com/photo/2017/04/27/08/29/man-2264825_1280.jpg"
-              alt="Second slide"
-            />
-            <Carousel.Caption>
-              <h5>Second slide label</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              <Button variant="outline-danger" size="lg">
-                Lire
-              </Button>{" "}
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://cdn.pixabay.com/photo/2023/09/11/14/19/weight-8246973_1280.jpg"
-              alt="Third slide"
-            />
-            <Carousel.Caption>
-              <h5>Third slide label</h5>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-              <Button variant="outline-danger" size="lg">
-                Lire
-              </Button>{" "}
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      </div>
-
-      <div className="articles">
-        <div className="title">
-          <h1>Derniers articles</h1>
-        </div>
-
-        <div className="cards">
-          <Row xs={1} md={2} className="g-4">
-            {Array.from({ length: 4 }).map((_, idx) => (
-              <Col key={idx}>
-                <Card>
-                  <Card.Img variant="top" src="" />
-                  <Card.Body>
-                    <Card.Title>Card title </Card.Title>
-                    <Card.Text>
-                      This is a longer card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
-                    </Card.Text>
-                    <Button variant="outline-danger" size="sm">
-                      Lire
-                    </Button>{" "}
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </div>
-      </div>
+      <form onSubmit={handleChange}>
+        <input
+          type="text"
+          name="content"
+          placeholder="post"
+          onChange={handleChange}
+        />
+        <button disabled={isLoading}>Poster</button>
+      </form>
     </>
   );
 };
